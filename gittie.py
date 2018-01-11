60b70912643d14936a5086064fccce1d2b1c3db7
prokajkristof <kiscoloshun@gmail.com>
2018-01-11 09:31:33 +0100

    shout

--- gittie.py
+++ gittie.py
@@ -3,6 +3,9 @@ def introduce():
 
 
 def joke():
-    "My girlfriend wanted me to treat her like a princess for her birthday. So I took her out, got her drunk, and crashed the car."
+    return "My girlfriend wanted me to treat her like a princess for her birthday. So I took her out, got her drunk, and crashed the car."
 def add(a, b):
     return a + b
+
+def shout():
+    return "shout"
