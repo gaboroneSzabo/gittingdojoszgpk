b276ea07a392ef4b9253dcf9cdfa642ba464f8a2
Gabor Szabo <puma678@gmail.com>
2018-01-11 09:34:12 +0100

    merge

--- gittie.py
+++ gittie.py
@@ -6,6 +6,11 @@ def joke():
     return "My girlfriend wanted me to treat her like a princess for her birthday. So I took her out, got her drunk, and crashed the car."
 def add(a, b):
     return a + b
+<<<<<<< Updated upstream
 
 def shout():
     return "shout"
+=======
+def shout():
+    print("aaaaaggggrrr")
+>>>>>>> Stashed changes
